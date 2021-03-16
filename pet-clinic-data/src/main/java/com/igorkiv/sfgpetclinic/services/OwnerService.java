7665ffad817f6399c456be8a740f9cf3faa95de6
Igor.Kiv <igor.kiv@gmail.com>
2021-03-16 22:27:28 +0600

    Adding static resources. Closes #17

--- pet-clinic-data/src/main/java/com/igorkiv/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/igorkiv/sfgpetclinic/services/OwnerService.java
@@ -2,7 +2,7 @@ package com.igorkiv.sfgpetclinic.services;
 
 import com.igorkiv.sfgpetclinic.model.Owner;
 
-public interface OwnerService extends CrudService<Owner, Long>
+public interface  OwnerService extends CrudService<Owner, Long>
 {
 
     Owner findByLastName(String lastName);
