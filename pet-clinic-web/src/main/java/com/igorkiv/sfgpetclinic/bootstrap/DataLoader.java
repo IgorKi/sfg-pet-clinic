b4fa664ea98f2d446e88917102797ea8788b8e86
Igor.Kiv <igor.kiv@gmail.com>
2021-03-18 19:18:36 +0600

    Added PetTypes to bootstrap closes. Closes #32

--- pet-clinic-web/src/main/java/com/igorkiv/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/igorkiv/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.igorkiv.sfgpetclinic.bootstrap;
 
 import com.igorkiv.sfgpetclinic.model.Owner;
+import com.igorkiv.sfgpetclinic.model.PerType;
 import com.igorkiv.sfgpetclinic.model.Vet;
 import com.igorkiv.sfgpetclinic.services.OwnerService;
+import com.igorkiv.sfgpetclinic.services.PetTypeService;
 import com.igorkiv.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PerType dog = new PerType();
+        dog.setName("Dog");
+        PerType saveDogPetType = petTypeService.save(dog);
+
+        PerType cat = new PerType();
+        cat.setName("Cat");
+        PerType saveCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Mihael");
