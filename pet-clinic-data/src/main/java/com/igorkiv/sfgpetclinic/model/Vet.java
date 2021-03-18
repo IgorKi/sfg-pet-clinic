3b02fa4af2556d67d2880fe858860f9a2e3ed0fe
Igor.Kiv <igor.kiv@gmail.com>
2021-03-18 18:20:48 +0600

    Added specialities, added to vet. Closes #27

--- pet-clinic-data/src/main/java/com/igorkiv/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/igorkiv/sfgpetclinic/model/Vet.java
@@ -1,4 +1,15 @@
 package com.igorkiv.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
