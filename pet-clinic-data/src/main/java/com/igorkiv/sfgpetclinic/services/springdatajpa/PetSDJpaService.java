53701c2084f8af4b7e1fc50672221aa17b0db1a1
Igor.Kiv <igor.kiv@gmail.com>
2021-03-23 18:31:44 +0600

    Added spring data jpa service for Pet. Closes #4

---
+++ pet-clinic-data/src/main/java/com/igorkiv/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,48 @@
+package com.igorkiv.sfgpetclinic.services.springdatajpa;
+
+import com.igorkiv.sfgpetclinic.model.Pet;
+import com.igorkiv.sfgpetclinic.repositories.PetRepository;
+import com.igorkiv.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
